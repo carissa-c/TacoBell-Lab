2820b9270bc440b053fd3bb639f64fd25e899bc8
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-02-16 07:58:15 -0600

    upload

---
+++ TacoBell API Lab/Controllers/DrinkController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TacoBell_API_Lab.Controllers
+{
+	public class DrinkController
+	{
+		public DrinkController()
+		{
+		}
+	}
+}
+
---
+++ TacoBell API Lab/Controllers/TacoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TacoBell_API_Lab.Controllers
+{
+    [Route("api/[controller]")]
+    public class TacoController : Controller
+    {
+        private TacoBellDbContext dbContext = new TacoBellDbContext();
+
+        [HttpGet]
+        public List<Taco> GetAll()
+        {
+            return dbContext.Tacos.ToList();
+        }
+        //ANY CHANGES TO DB (ADDING OR UPDATING) DON'T FORGET TO dbContext.SaveChanges()
+
+
+    }
+}
+
